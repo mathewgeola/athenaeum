b9adb719c352df5aed7916e11ff11a2d65dd2491
mathewgeola <mathewgeola@163.com>
2024-08-07 23:05:07 +0800

    run js

--- athenaeum/run/js.py
+++ athenaeum/run/js.py
@@ -9,7 +9,7 @@ def run_js_by_PyExecJS(js_code: str, func_name: str, *args: Any, **kwargs: Any) 
     return result
 
 
-def run_js_by_py_mini_racer(js_code: str, func_name: str, *args, **kwargs: Any) -> Any:
+def run_js_by_py_mini_racer(js_code: str, func_name: str, *args: Any, **kwargs: Any) -> Any:
     import py_mini_racer
 
     ctx = py_mini_racer.MiniRacer()
